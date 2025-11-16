144b191d655b934568d7b578b4c21c0b3f42b092
Kranceeee <12410724@mail.sustech.edu.cn>
2025-11-16 16:35:25 +0800

    Update C3P0Connector.java

--- UltimateVersion/C3P0Connector.java
+++ UltimateVersion/C3P0Connector.java
@@ -13,13 +13,11 @@ public class C3P0Connector {
         try {
             dataSource = new ComboPooledDataSource();
 
-            // 您的 PostgreSQL 連接參數
             dataSource.setDriverClass("org.postgresql.Driver");
             dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
             dataSource.setUser("postgres");
-            dataSource.setPassword("xhb77466708"); // 使用您的實際密碼
-
-            // C3P0 連接池配置 (較保守的預設值)
+            dataSource.setPassword("xhb77466708");
+            
             dataSource.setMinPoolSize(5);
             dataSource.setAcquireIncrement(5);
             dataSource.setMaxPoolSize(20);
@@ -29,8 +27,8 @@ public class C3P0Connector {
         }
     }
 
-    // 核心方法：與 HikariCP 的方法簽名保持一致
     public static Connection getConnection() throws SQLException {
         return dataSource.getConnection();
     }
+
 }
